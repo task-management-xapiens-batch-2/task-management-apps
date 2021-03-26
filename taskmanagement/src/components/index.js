dada77c933dd4c0ad97287a7bb808c2c29997cd6
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-27 03:12:24 +0700

    Components

--- taskmanagement/src/components/index.js
+++ taskmanagement/src/components/index.js
@@ -7,34 +7,31 @@ import {
   TextInput,
   ActivityIndicator,
 } from 'react-native';
-import CustomIcon from 'react-native-vector-icons/FontAwesome';
-import CIcon from 'react-native-vector-icons/SimpleLineIcons';
-
+import CustomIcon from 'react-native-vector-icons/SimpleLineIcons';
+import IconTabs from 'react-native-vector-icons/FontAwesome';
 const RColor = {
-  orange: '#DC7733',
-  lightOrange: '#F9E79F',
+  orange: '#F3AE46',
+  lightBlue: '#D4F3FD',
   blue: '#65AAEA',
-  white: '#FFFF',
-  green: '#5BA092',
-  grey: '#BEBAB3',
-  darkGrey: '#78746D',
-  lightGray: '#F8F2EE',
+  white: '#FFFFFF',
+  green: '#61AF62',
+  gray: '#BDBDBD',
 };
 
-const ACTIVE_TAB_COLOR = RColor.orange;
-const INACTIVE_TAB_COLOR = RColor.grey;
+const ACTIVE_TAB_COLOR = RColor.blue;
+const INACTIVE_TAB_COLOR = RColor.white;
 const Icon = ({name, focused, size, title}) => (
   <View
     style={{
       flexDirection: 'row',
       alignItems: 'center',
-      backgroundColor: focused ? RColor.lightOrange : null,
+      backgroundColor: focused ? RColor.white : null,
       borderRadius: 25,
-      height: 35,
+      height: '100%',
       paddingLeft: 10,
-      paddingRight: 10,
+      paddingRight: 10
     }}>
-    <CustomIcon
+    <IconTabs
       name={name}
       size={size}
       color={focused ? ACTIVE_TAB_COLOR : INACTIVE_TAB_COLOR}
@@ -43,20 +40,58 @@ const Icon = ({name, focused, size, title}) => (
       style={{
         fontWeight: 'bold',
         marginLeft: focused ? 5 : 0,
-        color: RColor.orange,
+        color: RColor.blue,
       }}>
       {focused ? title : ''}
     </Text>
   </View>
 );
+
+const RCard = (props, {item}) => {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={[styles.containerCard, props.CStyle]}>
+      <View>
+        <Text style={styles.txtTilteCard}>Judul</Text>
+        <Text style={styles.txtDateCard}>20-12-2021</Text>
+      </View>
+      <TouchableOpacity>
+        <RIcon name={props.iconName} size={20} />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
+};
+const RHeaderMain = props => {
+  return (
+    <View style={styles.containerHeaderMain}>
+      <Text style={styles.headerTitleMain}>{props.title}</Text>
+      <TouchableOpacity style={{position: 'absolute', right: '8%'}}>
+        <CustomIcon
+          style={{color: RColor.white}}
+          onPress={props.onPress}
+          name={props.iconName}
+          size={props.iconSize}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 const RHeader = props => {
   return (
     <View style={styles.containerHeader}>
-      <View style={{position: 'absolute'}}>
-        <RIcon onPress={props.onPress} name="arrow-left" size={20} />
-      </View>
-      <View style={styles.containerHeaderTitle}>
-        <Text style={{fontSize: 28, fontWeight: 'bold'}}>{props.title}</Text>
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={{position: 'absolute', left: '8%'}}>
+        <CustomIcon
+          onPress={props.onPress}
+          name={props.iconName}
+          size={25}
+          style={{color: RColor.white}}
+        />
+      </TouchableOpacity>
+      <View style={{alignSelf: 'center'}}>
+        <Text style={styles.headerTitle}>{props.title}</Text>
       </View>
     </View>
   );
@@ -64,29 +99,10 @@ const RHeader = props => {
 const RLoader = props => {
   return (
     <View style={[{alignSelf: 'center'}, props.styleLoader]}>
-      <ActivityIndicator size={props.size} color={RColor.orange} />
-      <Text style={{color: RColor.orange, fontWeight: 'bold'}}>
+      <ActivityIndicator size={props.size} color={RColor.blue} />
+      <Text style={{color: RColor.blue, fontWeight: 'bold'}}>
         {props.title}
       </Text>
-    </View>
-  );
-};
-const RSvg = props => {
-  return <SvgXml xml={props.xml} style={props.CStyle} />;
-};
-
-const RSkipRight = props => {
-  return (
-    <TouchableOpacity style={styles.txtSkipContainer} onPress={props.onPress}>
-      <Text style={styles.txtSkip}>{props.title}</Text>
-    </TouchableOpacity>
-  );
-};
-const RTitleSlider = props => {
-  return (
-    <View style={[styles.titleSliderContainer, props.CStyleContainer]}>
-      <Text style={[styles.txtSlideTitle, props.CStyle]}>{props.title}</Text>
-      <Text style={styles.txtSlideSubTitle}>{props.subTitle}</Text>
     </View>
   );
 };
@@ -107,34 +123,6 @@ const RButtonLoading = props => {
   );
 };
 
-const RDotFilled = props => {
-  const title = props.title;
-  return (
-    <View style={props.CStyle}>
-      {title == 'left' ? (
-        <View style={styles.dotContainer}>
-          <View style={styles.dotFilled} />
-          <View style={styles.dot} />
-          <View style={styles.dot} />
-        </View>
-      ) : title == 'mid' ? (
-        <View style={styles.dotContainer}>
-          <View />
-          <View style={styles.dot} />
-          <View style={styles.dotFilled} />
-          <View style={styles.dot} />
-        </View>
-      ) : (
-        <View style={styles.dotContainer}>
-          <View />
-          <View style={styles.dot} />
-          <View style={styles.dot} />
-          <View style={styles.dotFilled} />
-        </View>
-      )}
-    </View>
-  );
-};
 const RTextInput = props => {
   return (
     <View>
@@ -151,130 +139,69 @@ const RTextInput = props => {
   );
 };
 
-const RQuestion = props => {
-  return (
-    <TouchableOpacity style={styles.txtQContainer} onPress={props.onPress}>
-      <Text style={styles.txtQ}>{props.title}</Text>
-    </TouchableOpacity>
-  );
-};
-
 const RIcon = props => {
   return (
-    <TouchableOpacity onPress={props.onPress} style={styles.iconContainer}>
-      <CIcon name={props.name} size={props.size} style={{color: RColor.grey}} />
-    </TouchableOpacity>
+    <CustomIcon
+      name={props.name}
+      size={props.size}
+      style={{color: RColor.gray}}
+    />
   );
 };
 
-const RCategory = props => {
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: RColor.blue,
-        width: 54,
-        height: 24,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 15,
-        marginLeft: 10,
-        marginRight: 10,
-      }}>
-      <Text style={{color: RColor.white, fontSize: 12, fontStyle: 'normal'}}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  );
-};
 export {
+  RCard,
+  RHeaderMain,
   RHeader,
-  RCategory,
   RButtonLoading,
   RLoader,
-  RSvg,
   RIcon,
   Icon,
-  RQuestion,
   RTextInput,
   RColor,
   RButton,
-  RTitleSlider,
-  RDotFilled,
-  RSkipRight,
 };
 
 const styles = StyleSheet.create({
   // HEADER
   containerHeader: {
-    marginTop: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: '5%',
-    marginRight: '5%',
-  },
-  containerHeaderTitle: {
-    alignItems: 'center',
-    width: '100%',
     justifyContent: 'center',
+    height: 76,
+    backgroundColor: RColor.blue,
+    paddingLeft: '8%',
+    paddingRight: '8%',
   },
-  // ICON
-  iconContainer: {
-    borderColor: RColor.grey,
-    width: 45,
-    height: 45,
-    borderWidth: 1.5,
-    borderRadius: 45 / 2,
-    alignItems: 'center',
+  headerTitle: {
+    fontSize: 25,
+    color: RColor.white,
+  },
+  containerHeaderMain: {
     justifyContent: 'center',
+    height: 76,
+    backgroundColor: RColor.blue,
+    paddingLeft: '8%',
   },
-  // TXT QUESTION
-  txtQContainer: {
-    alignSelf: 'center',
-    marginTop: 15,
-    marginBottom: 15,
+  headerTitleMain: {
+    fontSize: 30,
+    color: RColor.white,
   },
-  txtQ: {
-    color: RColor.darkGrey,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+
   // TXT INPUT
   txtInput: {
     alignSelf: 'center',
-    width: '90%',
+    width: '80%',
     height: 50,
     borderRadius: 15,
-    borderWidth: 1,
     paddingLeft: 30,
-    borderColor: RColor.grey,
-  },
-  // TXT INPUT SEARCH
-  txtInputSearch: {
-    alignSelf: 'center',
-    width: '90%',
-    height: 50,
-    borderRadius: 15,
-    borderWidth: 1,
-    paddingLeft: 30,
-    borderColor: RColor.grey,
-  },
-  // SKIP
-  txtSkipContainer: {
-    marginRight: 10,
-    marginTop: 10,
-    alignSelf: 'flex-end',
-  },
-  txtSkip: {
-    color: RColor.grey,
-    fontSize: 16,
-    fontWeight: 'bold',
+    borderColor: RColor.blue,
+    backgroundColor: RColor.lightBlue,
   },
   // BUTTON
   btnContainer: {
-    width: '90%',
+    width: '80%',
     height: 55,
     borderRadius: 15,
-    backgroundColor: RColor.orange,
+    backgroundColor: RColor.blue,
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
@@ -284,34 +211,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: RColor.white,
   },
-  // SLIDER TEXT
-  titleSliderContainer: {
+  // CARD
+  containerCard: {
+    width: '90%',
+    height: 70,
+    borderRadius: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    marginBottom: 10,
+    backgroundColor: RColor.white,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    flexDirection: 'row',
   },
-  txtSlideTitle: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: '700',
-    width: '90%',
+  txtTilteCard: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
-  txtSlideSubTitle: {
-    textAlign: 'center',
-    width: '90%',
-  },
-  // SLIDER DOT
-  dotContainer: {flexDirection: 'row', alignSelf: 'center'},
-  dot: {
-    width: 6,
-    height: 6,
-    borderRadius: 3,
-    backgroundColor: RColor.grey,
-    marginLeft: 3,
-  },
-  dotFilled: {
-    width: 16,
-    height: 6,
-    borderRadius: 4,
-    backgroundColor: RColor.blue,
-    marginLeft: 3,
+  txtDateCard: {
+    color: RColor.green,
   },
 });
