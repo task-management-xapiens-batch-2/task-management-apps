0560cf8fdd34bdb05134e7ab5e33915272021ff3
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-27 08:42:37 +0700

    routing utils file

---
+++ taskmanagement/src/utils/index.js
@@ -0,0 +1,4 @@
+export {validateEmail as validateEmail} from './validate'
+export {validatePassword as validatePassword} from './validate'
+export {useEmailValidator as useEmailValidator} from './validator'
+export {usePasswordValidator as usePasswordValidator} from './validator'
