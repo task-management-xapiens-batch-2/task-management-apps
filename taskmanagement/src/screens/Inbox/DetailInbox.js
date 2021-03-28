466f8691280e9c744368b8bc43ccfef80451a3b8
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-28 17:40:16 +0700

    Screen Inbox and detail Inbox

--- taskmanagement/src/screens/Inbox/DetailInbox.js
+++ taskmanagement/src/screens/Inbox/DetailInbox.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {RHeader, RButtonLoading, RLoader, RButton, RColor} from '@reusable';
-const DetailInbox = ({navigation}) => {
+import {
+  RHeader,
+  RButtonLoading,
+  RLoader,
+  RDetailCard,
+  RButton,
+  RColor,
+} from '@reusable';
+const DetailInbox = ({navigation, route}) => {
+  const {
+    id,
+    project_id,
+    assignee,
+    title,
+    description,
+    start_date,
+    due_date,
+    attachment,
+    status,
+    is_read,
+  } = route.params;
+  const [loading, setLoading] = useState(false);
+
+  const takeTask = () => {
+    setLoading(true);
+  };
   return (
     <View style={styles.container}>
       <RHeader
@@ -9,22 +33,27 @@ const DetailInbox = ({navigation}) => {
         title="Inbox Detail"
         onPress={() => navigation.goBack()}
       />
-      <View style={styles.containerBody}>
-        <View style={styles.containerText}>
-          <View style={styles.loadingContainer}>
-            <View style={styles.loading}>
-              <Text style={styles.loadingText}>{(5 / 5) * 100} %</Text>
-            </View>
-          </View>
-
-          <Text style={styles.txtTitle}>AAA</Text>
-          <Text style={styles.txtDate}>20-12-2021</Text>
-          <Text style={styles.txtDetail}>test data detai</Text>
-        </View>
-        <View style={styles.containerBtn}>
-          <RButton title="TAKE THIS TASK" />
-        </View>
-      </View>
+      <RDetailCard
+        loading
+        title={title}
+        description={description}
+        sDate={start_date}
+        dDate={due_date}
+        CardStyle={{flex: 1}}
+      />
+      {loading ? (
+        <RButtonLoading CStyle={styles.btnColorDoing} />
+      ) : (
+        <RButton
+          title="TAKE THIS TASK"
+          onPress={() =>
+            updateStatusTask({
+              variables: {},
+            })
+          }
+          CStyle={{marginBottom:20}}
+        />
+      )}
     </View>
   );
 };
@@ -35,64 +64,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: RColor.lightBlue,
-  },
-  containerBody: {
-    flex: 1,
-    marginTop: '10%',
-    marginLeft: '5%',
-    marginRight: '5%',
-    marginBottom: '5%',
-    borderRadius: 20,
-    backgroundColor: RColor.white,
-  },
-  containerText: {
-    flex: 1,
-    borderRadius: 20,
-    alignItems: 'center',
-    paddingTop: 20,
-    backgroundColor: 'white',
-  },
-  loadingContainer: {
-    width: '80%',
-    height: 15,
-    borderWidth: 4,
-    borderRadius: 15 / 2,
-    marginBottom: 15,
-    borderColor: RColor.blue,
-    backgroundColor: RColor.lightBlue,
-    justifyContent: 'center',
-  },
-  loading: {
-    width: `${(5 / 5) * 100}%`,
-    height: 15,
-    borderRadius: 15 / 2,
-    backgroundColor: RColor.blue,
-    justifyContent: 'center',
-    paddingLeft: 15,
-  },
-  loadingText: {
-    color: RColor.white,
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  txtTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  txtDate: {
-    fontWeight: '500',
-    fontSize: 15,
-    color: RColor.gray,
-  },
-  txtDetail: {
-    fontWeight: '500',
-    fontSize: 15,
-    color: RColor.gray,
-    marginTop: 15,
-  },
-  containerBtn: {
-    height: 100,
-    marginBottom: 20,
-    justifyContent: 'center',
   },
 });
--- taskmanagement/src/screens/Inbox/Inbox.js
+++ taskmanagement/src/screens/Inbox/Inbox.js
@@ -1,20 +1,88 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {RHeader, RColor,RCard} from '@reusable';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {RHeader, RLoader, RColor, RIcon} from '@reusable';
+import {useLazyQuery, useQuery} from '@apollo/client';
+import {QUERY_TASKS} from '@config';
+import AsyncStorage from '@react-native-community/async-storage';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 const Inbox = ({navigation}) => {
+  const {data, error} = useQuery(QUERY_TASKS);
+  // const [taskList, {data, error}] = useLazyQuery(QUERY_TASKS);
+  // useEffect(async () => {
+  //   const rawDataUser = await AsyncStorage.getItem('data');
+  //   const dataUser = JSON.parse(rawDataUser);
+  //   if (dataUser !== null) {
+  //     taskList({
+  //       variables: {
+  //         assignee: dataUser.data.id,
+  //       },
+  //     });
+  //   } else {
+  //     console.log({error});
+  //   }
+  // }, []);
+
+  const RCard = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Detail Inbox', {
+            id: item.id,
+            project_id: item.project_id,
+            assignee: item.assignee,
+            title: item.title,
+            description: item.description,
+            start_date: item.start_date,
+            due_date: item.due_date,
+            attachment: item.attachment,
+            status: item.status,
+            is_read: item.is_read,
+          });
+        }}
+        style={styles.containerCard}>
+        <View>
+          <Text
+            style={[
+              styles.txtTilteCard,
+              {
+                fontWeight: isRead ? 'normal' : 'bold',
+                color: isRead ? RColor.gray : 'black',
+              },
+            ]}>
+            {item.title.length > 25
+              ? item.title.substring(0, 25) + ' .......'
+              : item.title}
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.txtDateCard}>{item.start_date} - </Text>
+            <Text style={styles.txtDateCard}>{item.due_date}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={{flex: 1, backgroundColor:RColor.lightBlue}}>
+    <View style={{flex: 1, backgroundColor: RColor.lightBlue}}>
       <RHeader
         onPress={() => navigation.goBack()}
         title={'Inbox'}
         iconName={'arrow-left'}
       />
-      <Text style={styles.txtTitle}> Task Title 0/2</Text>
-      <RCard onPress={()=>navigation.navigate("Inbox Detail")}/>
-      <RCard onPress={()=>navigation.navigate("Inbox Detail")}/>
-      <RCard onPress={()=>navigation.navigate("Inbox Detail")}/>
-      <RCard onPress={()=>navigation.navigate("Inbox Detail")}/>
-
+      {data ? (
+        <>
+          <Text style={styles.txtTitle}> Task Title 0/2</Text>
+          <FlatList
+            // .filter(f => f.status == 'approved')
+            data={data.findAllTask}
+            renderItem={RCard}
+            keyExtractor={item => item.id.toString()}
+          />
+        </>
+      ) : (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <RLoader size={30} title={'Load All Inbox....'} />
+        </View>
+      )}
     </View>
   );
 };
@@ -22,10 +90,29 @@ const Inbox = ({navigation}) => {
 export default Inbox;
 
 const styles = StyleSheet.create({
-    txtTitle:{
-        marginLeft:'5%',
-        marginTop:10,
-        marginBottom:10,
-        fontWeight:'bold'
-    }
+  txtTitle: {
+    marginLeft: '5%',
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  containerCard: {
+    width: '90%',
+    height: 90,
+    borderRadius: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    marginBottom: 10,
+    backgroundColor: RColor.white,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  txtTilteCard: {
+    fontSize: 16,
+  },
+  txtDateCard: {
+    color: RColor.green,
+  },
 });
