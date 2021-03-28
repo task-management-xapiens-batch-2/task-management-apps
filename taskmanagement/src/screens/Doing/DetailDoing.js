2ce15766eeac14c1c01c0d79d9ba54a9e14b3c1e
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-28 17:39:41 +0700

    Screen doing and detail Doing

--- taskmanagement/src/screens/Doing/DetailDoing.js
+++ taskmanagement/src/screens/Doing/DetailDoing.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {RHeader, RButtonLoading, RLoader, RButton, RColor} from '@reusable';
-const DetailDoing = ({navigation}) => {
+import {StyleSheet, View} from 'react-native';
+import {
+  RHeader,
+  RColor,
+  RButtonLoading,
+  RLoader,
+  RDetailCard,
+  RButton,
+} from '@reusable';
+const DetailDoing = ({navigation, route}) => {
+  const {pId, title, description, sDate, dDate} = route.params;
   return (
     <View style={styles.container}>
       <RHeader
@@ -9,17 +17,18 @@ const DetailDoing = ({navigation}) => {
         title="Detail Doing"
         onPress={() => navigation.goBack()}
       />
-      <View style={styles.containerBody}>
-        <View style={styles.containerText}>
-          <Text style={styles.txtTitle}>AAA</Text>
-          <Text style={styles.txtDate}>20-12-2021</Text>
-          <Text style={styles.txtDetail}>test data detai</Text>
-        </View>
-        <View style={styles.containerBtn}>
-          <RButton title="MAKE IT TODO"/>
-          <RButton title="MAKE IT DONE" CStyle={styles.btnColorDone} />
-        </View>
-      </View>
+
+      <RDetailCard
+        title={title}
+        description={description}
+        sDate={sDate}
+        dDate={dDate}
+        CardStyle={{flex:1}}
+        attachments={true}
+
+      />
+      <RButton title="MAKE IT TODO" />
+      <RButton title="MAKE IT DONE" CStyle={styles.btnColorDone} />
     </View>
   );
 };
@@ -33,42 +42,7 @@ const styles = StyleSheet.create({
   },
   btnColorDone: {
     backgroundColor: RColor.green,
-    marginTop:15
-  },
-  containerBody: {
-    flex: 1,
-    marginTop: '10%',
-    marginLeft: '5%',
-    marginRight: '5%',
-    marginBottom: '5%',
-    borderRadius: 20,
-    backgroundColor: RColor.white,
-  },
-  containerText: {
-    flex: 1,
-    borderRadius: 20,
-    alignItems:'center',
-    paddingTop:20,
-    backgroundColor: 'white',
-  },
-  txtTitle:{
-    fontSize:18,
-    fontWeight:'bold'
-  },
-  txtDate:{
-    fontWeight:'500',
-    fontSize:15,
-    color:RColor.gray
-  },
-  txtDetail:{
-    fontWeight:'500',
-    fontSize:15,
-    color:RColor.gray,
-    marginTop:15
-  },
-  containerBtn: {
-    height: 180,
-    marginBottom: 10,
-    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:20,
   },
 });
