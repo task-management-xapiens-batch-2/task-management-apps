de554389f2f973f669390b0204f7b29a920cd084
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2021-03-28 17:39:56 +0700

    Screen Done and detail Done

--- taskmanagement/src/screens/Done/DetailDone.js
+++ taskmanagement/src/screens/Done/DetailDone.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {RHeader, RButtonLoading, RLoader, RButton} from '@reusable';
-import {RColor} from '../../components';
-const DetailDone = ({navigation}) => {
+import {StyleSheet, View} from 'react-native';
+import {RHeader, RColor, RDetailCard} from '@reusable';
+const DetailDone = ({navigation, route}) => {
+  const {pId, title, description, sDate, dDate} = route.params;
   return (
     <View style={styles.container}>
       <RHeader
@@ -10,13 +10,15 @@ const DetailDone = ({navigation}) => {
         title="Detail Done"
         onPress={() => navigation.goBack()}
       />
-      <View style={styles.containerBody}>
-        <View style={styles.containerText}>
-          <Text style={styles.txtTitle}>AAA</Text>
-          <Text style={styles.txtDate}>20-12-2021</Text>
-          <Text style={styles.txtDetail}>test data detai</Text>
-        </View>
-      </View>
+
+      <RDetailCard
+        title={title}
+        description={description}
+        sDate={sDate}
+        dDate={dDate}
+        CardStyle={{flex:1}}
+        attachments
+      />
     </View>
   );
 };
@@ -25,41 +27,7 @@ export default DetailDone;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: RColor.lightBlue,
-  },
-  btnColor: {
-    backgroundColor: RColor.green,
-  },
-  containerBody: {
-    flex: 1,
-    marginTop: '10%',
-    marginLeft: '5%',
-    marginRight: '5%',
-    marginBottom: '5%',
-    borderRadius: 20,
-    backgroundColor: RColor.white,
-  },
-  containerText: {
-    flex: 1,
-    borderRadius: 20,
-    alignItems: 'center',
-    paddingTop: 20,
-    backgroundColor: 'white',
-  },
-  txtTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  txtDate: {
-    fontWeight: '500',
-    fontSize: 15,
-    color: RColor.gray,
-  },
-  txtDetail: {
-    fontWeight: '500',
-    fontSize: 15,
-    color: RColor.gray,
-    marginTop: 15,
   },
 });
--- taskmanagement/src/screens/Done/Done.js
+++ taskmanagement/src/screens/Done/Done.js
@@ -1,22 +1,167 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {RColor, RHeaderMain, RIcon, RCard} from '../../components';
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  BallIndicator,
+  BarIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+} from 'react-native-indicators';
+import {RColor, RHeaderMain, RLoader, RIcon} from '@reusable';
+import DocumentPicker from 'react-native-document-picker';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useLazyQuery} from '@apollo/client';
+import {QUERY_TASKS} from '@config';
 
 const Done = ({navigation}) => {
+  const [taskList, {data, error}] = useLazyQuery(QUERY_TASKS);
+  const [time, setTime] = useState(false);
+  const [id, setId] = useState('');
+  const [dataUser, setDataUser] = useState('');
+
+  useEffect(async () => {
+    const rawDataUser = await AsyncStorage.getItem('data');
+    const dataUser = JSON.parse(rawDataUser);
+    if (dataUser !== null) {
+      setId(dataUser.id);
+      setDataUser(dataUser.Login);
+      taskList({
+        variables: {
+          assignee: dataUser.data.id,
+        },
+      });
+    } else {
+      console.log({error});
+    }
+  }, []);
+  // setInterval(() => {
+  //   taskList({
+  //     variables: {
+  //       assignee: id,
+  //     },
+  //   });
+  // }, 300000);
+
+  setTimeout(() => {
+    setTime(true);
+  }, 4000);
+
+  const fetchTask = () => {
+    taskList({
+      variables: {
+        assignee: id,
+      },
+    });
+  };
+
+  const RCard = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Detail Done', {
+            id: item.id,
+            project_id: item.project_id,
+            assignee: item.assignee,
+            title: item.title,
+            description: item.description,
+            start_date: item.start_date,
+            due_date: item.due_date,
+            attachment: item.attachment,
+            status: item.status,
+            is_read: item.is_read,
+          })
+        }
+        style={styles.containerCard}>
+        <View>
+          <Text style={styles.txtTilteCard}>
+            {item.title.length > 25
+              ? item.title.substring(0, 25) + ' .......'
+              : item.title}
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.txtDateCard}>{item.start_date} - </Text>
+            <Text style={styles.txtDateCard}>{item.due_date}</Text>
+          </View>
+        </View>
+        <TouchableOpacity onPress={() => attachments()}>
+          <RIcon name={'paper-clip'} size={20} />
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
+  };
+
+  const attachments = async () => {
+    try {
+      const results = await DocumentPicker.pickMultiple({
+        type: [DocumentPicker.types.images],
+      });
+      for (const res of results) {
+        console.log(
+          res.uri,
+          res.type, // mime type
+          res.name,
+          res.size,
+        );
+      }
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <RHeaderMain title={'Alfian'}
-       iconName={'envelope'}
-       iconSize={30}
-       />
+      <RHeaderMain
+        title={
+          !dataUser.fullname ? (
+            dataUser.fullname
+          ) : (
+            <DotIndicator size={8} color={RColor.white} />
+          )
+        }
+        onPressRight={() => navigation.navigate('Inbox')}
+        onPress={() => fetchTask()}
+        iconName={'refresh'}
+        iconNameRight={'envelope'}
+        iconSize={30}
+        iconSizeRight={30}
+      />
       <Text style={styles.txtTitle}>TASK LIST</Text>
-      <RCard CStyle={styles.cardColor} onPress={()=>navigation.navigate('Detail Done')}/>
-      <RCard CStyle={styles.cardColor} onPress={()=>navigation.navigate('Detail Done')}/>
-      <RCard CStyle={styles.cardColor} onPress={()=>navigation.navigate('Detail Done')}/>
-      <RCard CStyle={styles.cardColor} onPress={()=>navigation.navigate('Detail Done')}/>
-      <RCard CStyle={styles.cardColor} onPress={()=>navigation.navigate('Detail Done')}/>
-
-
+      {!data && !time ? (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <RLoader size={30} title={'Load All Todo List....'} />
+        </View>
+      ) : time ? (
+        <View style={{flex: 1, justifyContent: 'center', alignSelf: 'center'}}>
+          <Image
+            source={require('../../img/box.png')}
+            style={{width: 90, height: 90, alignSelf: 'center'}}
+          />
+          <Text style={{color: RColor.blue, fontWeight: 'bold'}}>
+            OOPS !, your list task is Empty
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          data={data.listTaskWorker}
+          renderItem={RCard}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
     </View>
   );
 };
@@ -36,5 +181,26 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontWeight: 'bold',
   },
-  cardColor: {borderWidth: 3, borderColor: RColor.green},
+  containerCard: {
+    width: '90%',
+    height: 90,
+    borderRadius: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    marginBottom: 10,
+    backgroundColor: RColor.white,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderWidth: 4,
+    borderColor: RColor.green,
+  },
+  txtTilteCard: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  txtDateCard: {
+    color: RColor.green,
+  },
 });
